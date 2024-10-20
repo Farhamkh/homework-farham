8e8f8ea17a6e1b08d93c1d97c6cdde47edb2d12d
farhamkh <fk61462n@pace.edu>
2024-10-20 19:57:12 -0400

    Created Dashboard Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav} from 'react-bootstrap';
+import { FaHome, FaUser, FaCog, FaBell, FaEnvelope } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      Farham Homework Start
+    <div style={{display: 'flex'}}>
+      <div style={{ width: '250px', height: '800px', backgroundColor: 'gray', padding: '10px' }}>
+        <h1>Dashboard</h1>
+      </div>
+
+      <div>
+        <h1>Farham's React App</h1>
+      </div>
     </div>
   );
 }
