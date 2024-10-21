6d34ed4276440cf779b63a47b1f01cc4d601ff33
farhamkh <fk61462n@pace.edu>
2024-10-20 20:24:05 -0400

    Added the list to nav bar with the icons!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, Nav} from 'react-bootstrap';
-import { FaHome, FaUser, FaCog, FaBell, FaEnvelope } from 'react-icons/fa';
+import { FaHome, FaUser, FaCog, FaBell, FaEnvelope, FaShoppingCart, FaChartBar } from 'react-icons/fa';
 
 function App() {
   return (
     <div style={{display: 'flex'}}>
-      <div style={{ width: '250px', height: '800px', backgroundColor: 'gray', padding: '10px' }}>
+      <div style={{ width: '250px', height: '1000px', backgroundColor: 'gray', padding: '10px' }}>
         <h1>Dashboard</h1>
+        
+        <ul>
+          <li><FaHome/>Dashboards</li>
+          <li><FaUser/>Users</li>
+          <li><FaChartBar/>Analytics</li>
+          <li><FaShoppingCart/>Orders</li>
+          <li><FaCog/>Settings</li>
+        </ul>
       </div>
 
-      <div>
+      <div style={{marginLeft: '30px', padding: '15px'}}>
         <h1>Farham's React App</h1>
       </div>
     </div>
