ea7ad6abce32826e95ebf43dd68b8a66d0c9351f
farhamkh <fk61462n@pace.edu>
2024-10-20 23:50:58 -0400

    Added Performance Metrics, changed style of cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, Nav, Form, FormControl, Button, Row, Col, Card} from 'react-bootstrap';
-import { FaHome, FaUser, FaCog, FaBell, FaEnvelope, FaShoppingCart, FaChartBar, FaUsers, FaSearch} from 'react-icons/fa';
+import {Navbar, Nav, Form, FormControl, Button, Card, Table, ProgressBar} from 'react-bootstrap';
+import { FaHome, FaUser, FaCog, FaBell, FaShoppingCart, FaChartBar, FaUsers, FaSearch} from 'react-icons/fa';
 import { Bar } from 'react-chartjs-2'; 
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins } from 'chart.js';
+import { color } from 'chart.js/helpers';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -16,9 +17,9 @@ const chartData = {
       label: 'Monthly Sales',
       data: [500, 100, 750, 150, 120, 200],
       backgroundColor: [
-        "rgba(75,192,192,1)",
+        "rgba(33, 66, 211, 0.26)",
       ],
-      borderColor: "black",
+      borderColor: "rgba(33, 24, 113, 0.7)",
       borderWidth: 2
     }
   ]
@@ -61,7 +62,7 @@ const chartOptions = {
         </Navbar>
 
       
-
+<div  style={{width:'100%', height:'500px'}}>
           <Card>
             <Card.Body>
               <Card.Title>Total Users</Card.Title>
@@ -92,11 +93,70 @@ const chartOptions = {
 
           <Card>
             <Card.Body>
-              <Card.Title>Sales Overview</Card.Title>
-              <Bar data={chartData} options = {{responsive: true, plugins: { title: { display: true, text: 'Sales for 6 months'}}}}/>
+              <Card.Title>Performance Metrics</Card.Title>
+              <div>
+                CPU usage
+                <ProgressBar now={90} variant='danger'/> 
+              </div>
+              <div>
+                Memory usage
+                <ProgressBar now={50} variant='dark'/> 
+              </div>
+              <div>
+                Disk Usage
+                <ProgressBar now={90} variant='danger'/> 
+              </div>
             </Card.Body>
           </Card>
-      
+
+          <Card>
+            <Card.Body>
+              <Card.Title>Recent Activity</Card.Title>
+              <Table>
+                    <th>#</th>
+                    <th>User</th>
+                    <th>Action</th>
+                    <th>Date</th>
+                <tbody>
+                 
+                  <tr>
+                    <td>1</td>
+                    <td>Alex Man</td>
+                    <td>Purchased Item</td>
+                    <td>10/20/2024</td>
+                  </tr>
+
+                  <tr>
+                    <td>2</td>
+                    <td>Mark Stan</td>
+                    <td>Favorited Item</td>
+                    <td>10/18/2024</td>
+                  </tr>
+
+                  <tr>
+                    <td>3</td>
+                    <td>Allen Dancer</td>
+                    <td>Viewed Product Page</td>
+                    <td>10/10/2024</td>
+                  </tr>
+                </tbody>
+
+                <thread>
+                  
+                </thread>
+              </Table>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Body >
+              <Card.Title>Sales Overview</Card.Title>
+              <div style={{height:'600px'}}>
+                <Bar data={chartData} options = {{responsive: true, plugins: { title: { display: true, text: 'Sales for 6 months'}}}}/>
+              </div>            
+            </Card.Body>
+          </Card>
+        </div>
 
       </div>
     </div>
